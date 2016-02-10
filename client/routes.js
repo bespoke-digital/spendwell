f39e2241edca26cd7f833e2c08721c0c0c312e11
Aron Jones <aron.jones@gmail.com>
2016-02-10 17:30:25 -0500

    clean some old shit up

--- client/routes.js
+++ client/routes.js
@@ -31,7 +31,6 @@ export default (
         <Route path='dashboard/:year/:month' component={Dashboard} queries={viewerQuery}/>
 
         <Route path='goals/new' component={CreateGoal} queries={viewerQuery}/>
-        {/*<Route path='goals/:id' component={Goal} queries={viewerQuery}/>*/}
 
         <Route path='outgoing' component={Outgoing} queries={viewerQuery}/>
         <Route path='incoming' component={Incoming} queries={viewerQuery}/>
@@ -49,42 +48,9 @@ export default (
 );
 
 
-// import Logout from 'views/logout';
-// import Login from 'views/login';
-// import Signup from 'views/signup';
-// import Auth from 'views/auth';
-// import ConnectPlaid from 'views/connect';
-// import FinicitySearch from 'views/finicitySearch';
-// import FinicityConnect from 'views/finicityConnect';
 // import Buckets from 'views/buckets';
 // import Bucket from 'views/bucket';
 
-
-// export default (
-//   <Router history={browserHistory}>
-//     <Route component={Auth}>
-//       <Route path='/login' component={Login}/>
-//       <Route path='/logout' component={Logout}/>
-//       <Route path='/signup' component={Signup}/>
-//     </Route>
-
-//     <Route component={App}>
-//       <Route path='/incoming' component={Incoming}/>
-
-//       <Route path='/buckets' component={Buckets}/>
-//       <Route path='/buckets/:id' component={Bucket}/>
-
-//       <Route path='/goals/new' component={Goal}/>
-//       <Route path='/goals/:id' component={Goal}/>
-
-//       <Route path='/accounts' component={Accounts}/>
-//       <Route path='/accounts/:accountId' component={Accounts}/>
-//       <Route path='/connect/plaid' component={ConnectPlaid}/>
-//       <Route path='/connect/finicity' component={FinicitySearch}/>
-//       <Route path='/connect/finicity/:institutionId' component={FinicityConnect}/>
-//       <Route path='/connect/upload' component={Upload}/>
-
-//       <Route path='/categories' component={Categories}/>
-//     </Route>
-//   </Router>
-// );
+// <Route path='/buckets' component={Buckets}/>
+// <Route path='/buckets/:id' component={Bucket}/>
+// <Route path='/goals/:id' component={Goal}/>
