50938c9e0a4f49c307d7091f1ee810f511abc90e
Aron Jones <aron.jones@gmail.com>
2016-02-24 11:23:53 -0500

    new logo

--- client/components/header.js
+++ client/components/header.js
@@ -4,6 +4,7 @@ import Relay from 'react-relay';
 
 import Money from 'components/money';
 import logoWhite from 'img/logo-white.svg';
+import logoIconWhite from 'img/logo-icon-white.svg';
 import style from 'sass/components/header';
 
 
@@ -37,7 +38,8 @@ class Header extends Component {
           >☰</a>
         ) : null}
         <a className='brand mui--appbar-height mui--appbar-line-height' href='/app'>
-          <img src={logoWhite} alt='SpendWell'/>
+          <img src={logoWhite} alt='Spendwell' className='logo'/>
+          <img src={logoIconWhite} alt='Spendwell' className='icon'/>
         </a>
         {showSafeToSpend ?
           <div className='safe-to-spend'>
