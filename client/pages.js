0d4e1145b1ef0ed89051ee9fda5cd8447ddfba04
Aron Jones <aron.jones@gmail.com>
2016-02-25 16:51:19 -0500

    landing page design tweaks

--- client/pages.js
+++ client/pages.js
@@ -7,7 +7,6 @@ const landingCta = document.getElementById('landing-cta');
 if (landingCta) {
   landingCta.onclick = function(event) {
     window.mixpanel.track('Landing: CTA Clicked');
-    console.log('click');
 
     event.preventDefault();
 
@@ -18,5 +17,6 @@ if (landingCta) {
 
     formContainer.onclick = ()=> formContainer.style.display = 'none';
     form.onclick = (event)=> event.stopPropagation();
+    form.onsubmit = ()=> window.mixpanel.track('Landing: Email Submitted');
   };
 }
