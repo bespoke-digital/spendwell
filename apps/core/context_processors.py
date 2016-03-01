9d9374fe6642124d79764873f2c8743472fd27bf
Aron Jones <aron.jones@gmail.com>
2016-03-01 12:21:45 -0500

    abstract out analytics keys

--- apps/core/context_processors.py
+++ apps/core/context_processors.py
@@ -6,4 +6,6 @@ def settings(request):
     return {
         'REACT_HOT_LOADING': django_settings.REACT_HOT_LOADING,
         'MIXPANEL_PUBLIC_KEY': django_settings.MIXPANEL_PUBLIC_KEY,
+        'GOOGLE_ANALYTICS_KEY': django_settings.GOOGLE_ANALYTICS_KEY,
+        'FACEBOOK_PIXEL_KEY': django_settings.FACEBOOK_PIXEL_KEY,
     }
