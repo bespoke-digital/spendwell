e7977badb18b501189db33a09edadd2d447b4f4a
Aron Jones <aron.jones@gmail.com>
2016-03-03 10:36:55 -0500

    add mixpanel tracking to homepage

--- client/pages.js
+++ client/pages.js
@@ -6,21 +6,23 @@ const landingCta = document.getElementById('landing-cta');
 
 if (landingCta) {
   landingCta.onclick = function(event) {
+    event.preventDefault();
+
     window.mixpanel.track('Landing: CTA Clicked');
     window.fbq('track', 'Lead');
 
-    event.preventDefault();
-
     const formContainer = document.getElementById('landing-form-container');
-    const form = document.getElementById('landing-form');
-
     formContainer.style.display = 'block';
-
     formContainer.onclick = ()=> formContainer.style.display = 'none';
-    form.onclick = (event)=> event.stopPropagation();
-    form.onsubmit = ()=> {
-      window.mixpanel.track('Landing: Email Submitted');
-      window.fbq('track', 'CompleteRegistration');
-    };
+  };
+}
+
+const form = document.getElementById('landing-form');
+
+if (form) {
+  form.onclick = (event)=> event.stopPropagation();
+  form.onsubmit = ()=> {
+    window.mixpanel.track('Landing: Email Submitted');
+    window.fbq('track', 'CompleteRegistration');
   };
 }
