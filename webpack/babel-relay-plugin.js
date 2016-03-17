c8b50d9c06aa74aefd470afc202ec24cd6e11dcd
Aron Jones <aron.jones@gmail.com>
2016-03-17 12:07:41 -0400

    use python bin from node

--- webpack/babel-relay-plugin.js
+++ webpack/babel-relay-plugin.js
@@ -4,7 +4,7 @@ var babelRelayPlugin = require('babel-relay-plugin');
 var childProcess = require('child_process');
 var path = require('path');
 
-childProcess.execSync(path.normalize('./manage.py') + ' export_schema');
+childProcess.execSync('python ' + path.normalize('./manage.py') + ' export_schema');
 
 var schema = require('../spendwell/schema.json');
 
