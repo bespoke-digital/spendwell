59b479988ae00396b5813413d2000cd69aa8f0ec
Aron Jones <aron.jones@gmail.com>
2016-04-04 17:40:17 -0400

    pass kwargs through create user

--- apps/users/models.py
+++ apps/users/models.py
@@ -10,17 +10,17 @@ from apps.core.utils import months_ago, this_month
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, password=None, **kwargs):
         if not email:
             raise ValueError('Users must have an email address')
 
-        user = self.model(email=self.normalize_email(email))
+        user = self.model(email=self.normalize_email(email), **kwargs)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email, password=password)
+    def create_superuser(self, email, password, **kwargs):
+        user = self.create_user(email, password=password, **kwargs)
         user.is_admin = True
         user.save()
         return user
