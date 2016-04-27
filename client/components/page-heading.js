f48931e072e1489ab63519a2ff57abe1e5d7f7ac
Aron Jones <aron.jones@gmail.com>
2016-04-27 16:35:15 -0400

    remove dead imports

--- client/components/page-heading.js
+++ client/components/page-heading.js
@@ -1,13 +1,5 @@
 
 import { Component } from 'react';
-import Relay from 'react-relay';
-import { browserHistory } from 'react-router';
-
-import { handleMutationError } from 'utils/network-layer';
-import App from 'components/app';
-import GoalForm from 'components/goal-form';
-import { CreateGoalMutation } from 'mutations/goals';
-
 import styles from 'sass/components/page-heading.scss';
 
 
