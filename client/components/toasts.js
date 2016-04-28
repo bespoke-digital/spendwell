d112446960b1abf448de939fb200ec9a91e0052a
Aron Jones <aron.jones@gmail.com>
2016-04-28 17:33:51 -0400

    toa WIP

--- client/components/toasts.js
+++ client/components/toasts.js
@@ -4,15 +4,17 @@ import { TransitionMotion, spring } from 'react-motion';
 import styles from 'sass/components/toasts';
 
 export default ({ toasts, className, ...props })=>
-  <ul className={`${styles.root} ${className || ''}`} {...props}>
-    <TransitionMotion
-      willLeave={()=> ({ opacity: spring(0), marginTop: spring(0) })}
-      willEnter={()=> ({ opacity: 0, marginBottom: -20 })}
-      styles={toasts.map((toast)=> ({
-        toast,
-        styles: { opacity: 1, marginBottom: 0, marginTop: 20 },
-      }))}
-    >{(styles)=> styles.map(({ toast, styles })=>
-      <li key={toast.id} styles={styles}>{toast.message}</li>
-    )}</TransitionMotion>
-  </ul>;
+  <TransitionMotion
+    willLeave={()=> ({ opacity: spring(0), marginTop: spring(0) })}
+    willEnter={()=> ({ opacity: 0, marginBottom: -20 })}
+    styles={toasts.map((toast)=> ({
+      toast,
+      styles: { opacity: 1, marginBottom: 0, marginTop: 20 },
+    }))}
+  >{(styles)=> (
+    <ul className={`${styles.root} ${className || ''}`} {...props}>
+      {styles.map(({ toast, styles })=>
+        <li key={toast.id} style={styles}>{toast.message}</li>
+      )}
+    </ul>
+  )}</TransitionMotion>;
