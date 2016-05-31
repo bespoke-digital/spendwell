b06c6737e62e83ec9b9ee37e25bfc384f62b1147
Wasee Malik <malik.w.as@gmail.com>
2016-05-31 17:13:25 -0400

    change to schema path

--- webpack/babel-relay-plugin.js
+++ webpack/babel-relay-plugin.js
@@ -5,7 +5,7 @@ var childProcess = require('child_process');
 var path = require('path');
 
 var schema = JSON.parse(childProcess.execSync(
-  'python ' + path.resolve('./manage.py') + ' export_schema'
+  'python manage.py export_schema';
 ));
 
 module.exports = babelRelayPlugin(schema);
