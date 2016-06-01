beaafbed0ada811dc3459a98461c98405035a8e6
Aron Jones <aron.jones@gmail.com>
2016-06-01 14:38:59 -0400

    remove js account create mutation

--- client/mutations/accounts.js
+++ client/mutations/accounts.js
@@ -2,56 +2,6 @@
 import Relay from 'react-relay';
 
 
-export class CreateAccountMutation extends Relay.Mutation {
-  static fragments = {
-    institution: ()=> Relay.QL`
-      fragment on InstitutionNode {
-        id
-      }
-    `,
-  };
-
-  getMutation() {
-    return Relay.QL`mutation { createAccount }`;
-  }
-
-  getVariables() {
-    return {
-      name: this.props.name,
-      institutionId: this.props.institution.id,
-    };
-  }
-
-  getFatQuery() {
-    return Relay.QL`
-      fragment on CreateAccountMutation {
-        institution { accounts }
-        accountEdge
-      }
-    `;
-  }
-
-  getConfigs() {
-    return [{
-      type: 'RANGE_ADD',
-      parentName: 'institution',
-      parentID: this.props.institution.id,
-      connectionName: 'accounts',
-      edgeName: 'accountEdge',
-      rangeBehaviors: { '': 'append' },
-    }];
-  }
-
-  getOptimisticResponse() {
-    return {
-      account: {
-        name: this.props.name,
-      },
-    };
-  }
-}
-
-
 export class DisableAccountMutation extends Relay.Mutation {
   static fragments = {
     account: ()=> Relay.QL`
