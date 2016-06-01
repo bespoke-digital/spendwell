1be2fdee1b1fb4d98538a22825f94a2e6456359a
Aron Jones <aron.jones@gmail.com>
2016-06-01 11:15:58 -0400

    address env direct

--- webpack/babel-relay-plugin.js
+++ webpack/babel-relay-plugin.js
@@ -3,6 +3,6 @@
 var babelRelayPlugin = require('babel-relay-plugin');
 var childProcess = require('child_process');
 
-var schema = JSON.parse(childProcess.execSync('python manage.py export_schema'));
+var schema = JSON.parse(childProcess.execSync('$VIRTUAL_ENV/bin/python ./manage.py export_schema'));
 
 module.exports = babelRelayPlugin(schema);
