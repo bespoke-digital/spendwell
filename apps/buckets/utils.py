757cdc55cc5732a8aa4449ae52d88b37f86335fb
Aron Jones <aron.jones@gmail.com>
2016-06-06 14:46:50 -0400

    remove month delay for bill detect

--- apps/buckets/utils.py
+++ apps/buckets/utils.py
@@ -37,7 +37,7 @@ def bill_month_match(transaction, month_start, ago):
 
 
 def autodetect_bills(user):
-    full_month_end = this_month() - relativedelta(months=1)
+    full_month_end = this_month()
     full_month_start = full_month_end - relativedelta(months=1)
 
     for transaction in Transaction.objects.filter(
