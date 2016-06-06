1d28b5813f35c91d23171cbb38f35e2442b9e988
Karan Bhandari <karan7752@gmail.com>
2016-06-06 14:10:01 -0400

    Required changes made to the Goals tests

--- apps/goals/tests.py
+++ apps/goals/tests.py
@@ -1,8 +1,6 @@
-
-from apps.core.tests import SWTestCase
 from decimal import Decimal
 
-
+from apps.core.tests import SWTestCase
 from apps.users.factories import UserFactory
 from apps.goals.factories import GoalFactory
 from apps.core.utils import node_id_from_instance
@@ -11,8 +9,8 @@ from .models import Goal
 
 
 class GoalsTestCase(SWTestCase):
-    # GraphQl string input
-    goal_graph_query = '''{
+    goal_graph_query = \
+        '''{
             viewer {
                 goals {
                     edges {
@@ -41,7 +39,7 @@ class GoalsTestCase(SWTestCase):
                     'clientMutationId': '123',
                     'name': 'Spend-Very Well',
                     'monthlyAmount': 500
-                 }
+                }
             },
             user=owner,
         )
@@ -58,7 +56,7 @@ class GoalsTestCase(SWTestCase):
 
         self.assertEqual(
             len(result.data['viewer']['goals']['edges']),
-            1
+            1,
         )
         # checking key and values specifics using GraphQl
         self.assertTrue('viewer' in result.data)
@@ -68,10 +66,14 @@ class GoalsTestCase(SWTestCase):
         self.assertTrue('id' in result.data['viewer']['goals']['edges'][0]['node'])
         self.assertTrue('monthlyAmount' in result.data['viewer']['goals']['edges'][0]['node'])
         self.assertEqual(
-            result.data['viewer']['goals']['edges'][0]['node']['monthlyAmount'], -500)
+            result.data['viewer']['goals']['edges'][0]['node']['monthlyAmount'],
+            -500,
+             )
         self.assertTrue('name' in result.data['viewer']['goals']['edges'][0]['node'])
         self.assertEqual(
-            result.data['viewer']['goals']['edges'][0]['node']['name'], 'Spend-Very Well')
+            result.data['viewer']['goals']['edges'][0]['node']['name'],
+            'Spend-Very Well',
+            )
 
         # checking value specs form DB
         goal = Goal.objects.get(name='Spend-Very Well')
@@ -90,16 +92,16 @@ class GoalsTestCase(SWTestCase):
               updateGoal(input: $input){
                 clientMutationId,
             }
-        }''', variable_values={
-                'input': {
-                     'clientMutationId': 'abcd123',
-                     'goalId': node_id_from_instance(goal),
-                     'name': 'SpendIt-Very Well',
-                     'monthlyAmount': -1000
-                     }
+        }''',   variable_values={
+                    'input': {
+                        'clientMutationId': 'abcd123',
+                        'goalId': node_id_from_instance(goal),
+                        'name': 'SpendIt-Very Well',
+                        'monthlyAmount': -1000,
+                    }
                 },
-            user=owner,
-        )
+                user=owner,
+            )
         resultForDB = self.graph_query(
             self.goal_graph_query,
             user=owner,
@@ -113,7 +115,7 @@ class GoalsTestCase(SWTestCase):
 
         self.assertEqual(
             len(resultForDB.data['viewer']['goals']['edges']),
-            1
+            1,
         )
         # checking key and values specifics using GraphQl
         self.assertTrue('viewer' in resultForDB.data)
@@ -123,10 +125,14 @@ class GoalsTestCase(SWTestCase):
         self.assertTrue('id' in resultForDB.data['viewer']['goals']['edges'][0]['node'])
         self.assertTrue('monthlyAmount' in resultForDB.data['viewer']['goals']['edges'][0]['node'])
         self.assertEqual(
-            resultForDB.data['viewer']['goals']['edges'][0]['node']['monthlyAmount'], -1000)
+            resultForDB.data['viewer']['goals']['edges'][0]['node']['monthlyAmount'],
+            -1000,
+            )
         self.assertTrue('name' in resultForDB.data['viewer']['goals']['edges'][0]['node'])
         self.assertEqual(
-            resultForDB.data['viewer']['goals']['edges'][0]['node']['name'], 'SpendIt-Very Well')
+            resultForDB.data['viewer']['goals']['edges'][0]['node']['name'],
+            'SpendIt-Very Well',
+             )
 
         # checking value specs form DB
         goal = Goal.objects.get(name='SpendIt-Very Well')
@@ -146,10 +152,10 @@ class GoalsTestCase(SWTestCase):
                 clientMutationId,
             }
         }''', variable_values={
-                'input': {
-                     'clientMutationId': 'ABCDabcd123',
-                     'goalId': node_id_from_instance(goal),
-                     }
+                    'input': {
+                        'clientMutationId': 'ABCDabcd123',
+                        'goalId': node_id_from_instance(goal),
+                    }
                 },
             user=owner,
         )
@@ -168,7 +174,7 @@ class GoalsTestCase(SWTestCase):
 
         self.assertEqual(
             len(resultForDB.data['viewer']['goals']['edges']),
-            0
+            0,
         )
         self.assertTrue('viewer' in resultForDB.data)
         self.assertTrue('goals' in resultForDB.data['viewer'])
