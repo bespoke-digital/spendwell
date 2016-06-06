9c1035d10f0ff4ceb414126d866f6db12311a58e
Karan Bhandari <karan7752@gmail.com>
2016-06-06 16:14:43 -0400

    identations fixed

--- apps/goals/tests.py
+++ apps/goals/tests.py
@@ -1,16 +1,15 @@
 from decimal import Decimal
 
 from apps.core.tests import SWTestCase
-from apps.users.factories import UserFactory
-from apps.goals.factories import GoalFactory
 from apps.core.utils import node_id_from_instance
+from apps.goals.factories import GoalFactory
+from apps.users.factories import UserFactory
 
 from .models import Goal
 
 
 class GoalsTestCase(SWTestCase):
-    goal_graph_query = \
-        '''{
+    goal_graph_query = '''{
             viewer {
                 goals {
                     edges {
@@ -22,19 +21,22 @@ class GoalsTestCase(SWTestCase):
                     }
                 }
             }
-        }'''
+    }'''
 
     def test_creating_goals(self):
 
         owner = UserFactory.create()
 
         # Checking create goal here
-        result = self.graph_query('''
-            mutation ($input: CreateGoalMutationInput!){
-              createGoal(input: $input){
-                clientMutationId,
-            }
-        }''', variable_values={
+        result = self.graph_query(
+            '''
+                mutation ($input: CreateGoalMutationInput!){
+                    createGoal(input: $input){
+                        clientMutationId,
+                    }
+                }
+            ''',
+            variable_values={
                 'input': {
                     'clientMutationId': '123',
                     'name': 'Spend-Very Well',
@@ -63,17 +65,25 @@ class GoalsTestCase(SWTestCase):
         self.assertTrue('goals' in result.data['viewer'])
         self.assertTrue('edges' in result.data['viewer']['goals'])
         self.assertTrue('node' in result.data['viewer']['goals']['edges'][0])
-        self.assertTrue('id' in result.data['viewer']['goals']['edges'][0]['node'])
-        self.assertTrue('monthlyAmount' in result.data['viewer']['goals']['edges'][0]['node'])
+        self.assertTrue(
+            'id' in result.data['viewer']['goals']['edges'][0]['node']
+        )
+        self.assertTrue(
+            'monthlyAmount'
+            in
+            result.data['viewer']['goals']['edges'][0]['node']
+        )
         self.assertEqual(
             result.data['viewer']['goals']['edges'][0]['node']['monthlyAmount'],
             -500,
-             )
-        self.assertTrue('name' in result.data['viewer']['goals']['edges'][0]['node'])
+        )
+        self.assertTrue(
+            'name' in result.data['viewer']['goals']['edges'][0]['node']
+        )
         self.assertEqual(
             result.data['viewer']['goals']['edges'][0]['node']['name'],
             'Spend-Very Well',
-            )
+        )
 
         # checking value specs form DB
         goal = Goal.objects.get(name='Spend-Very Well')
@@ -87,52 +97,74 @@ class GoalsTestCase(SWTestCase):
         goal = GoalFactory.create(monthly_amount=Decimal('-500'), owner=owner)
 
         # Checking updating goal Graphiql specs here
-        result = self.graph_query('''
+        result = self.graph_query(
+            '''
             mutation ($input: UpdateGoalMutationInput!){
-              updateGoal(input: $input){
-                clientMutationId,
+                updateGoal(input: $input){
+                    clientMutationId,
+                }
             }
-        }''',   variable_values={
-                    'input': {
-                        'clientMutationId': 'abcd123',
-                        'goalId': node_id_from_instance(goal),
-                        'name': 'SpendIt-Very Well',
-                        'monthlyAmount': -1000,
-                    }
-                },
-                user=owner,
-            )
-        resultForDB = self.graph_query(
+            ''',
+            variable_values={
+                'input': {
+                    'clientMutationId': 'abcd123',
+                    'goalId': node_id_from_instance(goal),
+                    'name': 'SpendIt-Very Well',
+                    'monthlyAmount': -1000,
+                }
+            },
+            user=owner,
+        )
+        result_for_db = self.graph_query(
             self.goal_graph_query,
             user=owner,
         )
         # Checks the number of Goals updated here
         self.assertEqual(len(result.data['updateGoal']), 1)
         # Validates clientMutationId of the goal
-        self.assertEqual(result.data['updateGoal']['clientMutationId'], 'abcd123')
+        self.assertEqual(
+            result.data['updateGoal']['clientMutationId'],
+            'abcd123'
+        )
         self.assertTrue('updateGoal' in result.data)
         self.assertTrue('clientMutationId' in result.data['updateGoal'])
 
         self.assertEqual(
-            len(resultForDB.data['viewer']['goals']['edges']),
+            len(result_for_db.data['viewer']['goals']['edges']),
             1,
         )
         # checking key and values specifics using GraphQl
-        self.assertTrue('viewer' in resultForDB.data)
-        self.assertTrue('goals' in resultForDB.data['viewer'])
-        self.assertTrue('edges' in resultForDB.data['viewer']['goals'])
-        self.assertTrue('node' in resultForDB.data['viewer']['goals']['edges'][0])
-        self.assertTrue('id' in resultForDB.data['viewer']['goals']['edges'][0]['node'])
-        self.assertTrue('monthlyAmount' in resultForDB.data['viewer']['goals']['edges'][0]['node'])
+        self.assertTrue('viewer' in result_for_db.data)
+        self.assertTrue('goals' in result_for_db.data['viewer'])
+        self.assertTrue('edges' in result_for_db.data['viewer']['goals'])
+        self.assertTrue(
+            'node'
+            in
+            result_for_db.data['viewer']['goals']['edges'][0]
+        )
+        self.assertTrue(
+            'id'
+            in
+            result_for_db.data['viewer']['goals']['edges'][0]['node']
+        )
+        self.assertTrue(
+            'monthlyAmount'
+            in
+            result_for_db.data['viewer']['goals']['edges'][0]['node']
+        )
         self.assertEqual(
-            resultForDB.data['viewer']['goals']['edges'][0]['node']['monthlyAmount'],
+            result_for_db.data['viewer']['goals']['edges'][0]['node']['monthlyAmount'],
             -1000,
-            )
-        self.assertTrue('name' in resultForDB.data['viewer']['goals']['edges'][0]['node'])
+        )
+        self.assertTrue(
+            'name'
+            in
+            result_for_db.data['viewer']['goals']['edges'][0]['node']
+        )
         self.assertEqual(
-            resultForDB.data['viewer']['goals']['edges'][0]['node']['name'],
+            result_for_db.data['viewer']['goals']['edges'][0]['node']['name'],
             'SpendIt-Very Well',
-             )
+        )
 
         # checking value specs form DB
         goal = Goal.objects.get(name='SpendIt-Very Well')
@@ -146,39 +178,45 @@ class GoalsTestCase(SWTestCase):
         goal = GoalFactory.create(monthly_amount=Decimal('-750'), owner=owner)
 
         # Checking deleting goal Graphiql specs here
-        result = self.graph_query('''
+        result = self.graph_query(
+            '''
             mutation ($input: DeleteGoalMutationInput!){
-              deleteGoal(input: $input){
-                clientMutationId,
+                deleteGoal(input: $input){
+                    clientMutationId,
+                }
             }
-        }''', variable_values={
-                    'input': {
-                        'clientMutationId': 'ABCDabcd123',
-                        'goalId': node_id_from_instance(goal),
-                    }
-                },
+            ''',
+            variable_values={
+                'input': {
+                    'clientMutationId': 'ABCDabcd123',
+                    'goalId': node_id_from_instance(goal),
+                }
+            },
             user=owner,
         )
-        resultForDB = self.graph_query(
+        result_for_db = self.graph_query(
             self.goal_graph_query,
             user=owner,
         )
         # Checks the number of Goals deleted here
         self.assertEqual(len(result.data['deleteGoal']), 1)
         # Validates clientMutationId of the goal
-        self.assertEqual(result.data['deleteGoal']['clientMutationId'], 'ABCDabcd123')
+        self.assertEqual(
+            result.data['deleteGoal']['clientMutationId'],
+            'ABCDabcd123'
+        )
 
         # checking key and values specifics using GraphQl
         self.assertTrue('deleteGoal' in result.data)
         self.assertTrue('clientMutationId' in result.data['deleteGoal'])
 
         self.assertEqual(
-            len(resultForDB.data['viewer']['goals']['edges']),
+            len(result_for_db.data['viewer']['goals']['edges']),
             0,
         )
-        self.assertTrue('viewer' in resultForDB.data)
-        self.assertTrue('goals' in resultForDB.data['viewer'])
-        self.assertTrue('edges' in resultForDB.data['viewer']['goals'])
+        self.assertTrue('viewer' in result_for_db.data)
+        self.assertTrue('goals' in result_for_db.data['viewer'])
+        self.assertTrue('edges' in result_for_db.data['viewer']['goals'])
 
         # checking value specs form DB
         goal = Goal.objects.all()
