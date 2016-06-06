ef996ce05e0b5419083b4b37c7ab34f9be5833a4
Wasee Malik <wmlk@github.com>
2016-06-06 11:41:57 -0400

    lint and formatting

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,4 +1,3 @@
-from django.test import TestCase
 from decimal import Decimal
 
 from apps.core.tests import SWTestCase
@@ -9,9 +8,13 @@ from apps.transactions.factories import TransactionFactory
 
 from .models import Account
 
-# Series of tests that check for balance in accounts based on the 'disabled' boolean in the accounts model
-# Each enableAccount or disableAccount mutation changes the 'disabled' boolean
+
 class EnableDisableFlagsTestCase(SWTestCase):
+    '''
+    Tests which check for balance in accounts based on the disabled boolean in the accounts model.
+    Each enableAccount or disableAccount mutation changes the disabled boolean.
+    '''
+
     # GraphQL string input for querying accounts. Stored for readability and repeated use.
     accounts_graph_query = '''{
         viewer {
@@ -27,28 +30,37 @@ class EnableDisableFlagsTestCase(SWTestCase):
         }
     }'''
 
-    # Function that returns the total current balance from activated accounts, based on the 'disabled' boolean.
-    def enabled_accounts_total(self, accounts_JSON ):
-            total = 0
+    def enabled_accounts_total(self, accounts_JSON):
+        'Returns the total current balance from activated accounts, based on the disabled flag.'
 
-            for account in accounts_JSON:
-                if account['node']['disabled'] == False:
-                    total += account['node']['currentBalance']
+        total = 0
 
-            return total
+        for account in accounts_JSON:
+            if not account['node']['disabled']:
+                total += account['node']['currentBalance']
 
-    # Basic GraphQL query after creating two accounts to see if they exist.
+        return total
+
     def test_flags_and_balance_enabled_accounts(self):
+        'Basic GraphQL query after creating two accounts to see if they exist.'
+
         owner = UserFactory.create()
         AccountFactory.create(owner=owner, current_balance=Decimal(100))
         AccountFactory.create(owner=owner, current_balance=Decimal(200))
 
-        result = self.graph_query(self.accounts_graph_query, user=owner).data['viewer']['accounts']['edges']
+        result = self.graph_query(
+            self.accounts_graph_query,
+            user=owner,
+        ).data['viewer']['accounts']['edges']
+
         self.assertTrue(self.enabled_accounts_total(result) == 30000)
 
-    # A series of tests to see the change in total current_balance after enabling and disabling two accounts with
-    # two different current_balance values.
     def test_flags_and_balance_disabled_accounts(self):
+        '''
+        Test to confirm change in total current_balance after enabling/disabling two accounts with
+        different current_balance values.
+        '''
+
         owner = UserFactory.create()
         account_1 = AccountFactory.create(owner=owner, current_balance=Decimal(100))
         account_2 = AccountFactory.create(owner=owner, current_balance=Decimal(200))
@@ -76,11 +88,15 @@ class EnableDisableFlagsTestCase(SWTestCase):
             user=owner,
         )
 
-        # Check if current_balance total is be 200 after account_1 is disabled.
-        result = self.graph_query(self.accounts_graph_query, user=owner).data['viewer']['accounts']['edges']
+        # Check if current_balance total is 200 after account_1 is disabled.
+        result = self.graph_query(
+            self.accounts_graph_query,
+            user=owner,
+        ).data['viewer']['accounts']['edges']
+
         self.assertTrue(self.enabled_accounts_total(result) == 20000)
 
-        # Check if GraphQL mutation is reflected in database by doing a database query.
+        # Check if mutation is reflected in database by doing a database query.
         account_1 = Account.objects.get(current_balance=100)
         self.assertTrue(account_1.disabled)
 
@@ -108,10 +124,14 @@ class EnableDisableFlagsTestCase(SWTestCase):
         )
 
         # Check if current_balance total is be 0 after account_1 and account_2 are disabled.
-        result = self.graph_query(self.accounts_graph_query, user=owner).data['viewer']['accounts']['edges']
+        result = self.graph_query(
+            self.accounts_graph_query,
+            user=owner,
+        ).data['viewer']['accounts']['edges']
+
         self.assertTrue(self.enabled_accounts_total(result) == 0)
 
-        # Check if GraphQL mutation is reflected in database by doing a database query.
+        # Check if mutation is reflected in database by doing a database query.
         account_2 = Account.objects.get(current_balance=200)
         self.assertTrue(account_2.disabled)
 
@@ -138,13 +158,17 @@ class EnableDisableFlagsTestCase(SWTestCase):
             user=owner,
         )
 
-        # Check if current_balance total is be 100 after account_2 is disabled.
-        result = self.graph_query(self.accounts_graph_query, user=owner).data['viewer']['accounts']['edges']
+        # Check if current_balance total is 100 after account_2 is disabled.
+        result = self.graph_query(
+            self.accounts_graph_query,
+            user=owner,
+        ).data['viewer']['accounts']['edges']
+
         self.assertTrue(self.enabled_accounts_total(result) == 10000)
-        
-        # Check if GraphQL mutation is reflected in database by doing a database query.
+
+        # Check if mutation is reflected in database by doing a database query.
         account_1 = Account.objects.get(current_balance=100)
-        self.assertTrue(account_1.disabled == False)
+        self.assertFalse(account_1.disabled)
 
         self.graph_query(
             '''
@@ -169,20 +193,25 @@ class EnableDisableFlagsTestCase(SWTestCase):
             user=owner,
         )
 
-        # Check if current_balance total is 300 after no accounts are disabled. 
-        result = self.graph_query(self.accounts_graph_query, user=owner).data['viewer']['accounts']['edges']
+        # Check if current_balance total is 300 after no accounts are disabled.
+        result = self.graph_query(
+            self.accounts_graph_query,
+            user=owner,
+        ).data['viewer']['accounts']['edges']
         self.assertTrue(self.enabled_accounts_total(result) == 30000)
 
-        # Check if GraphQL mutation is reflected in database by doing a database query.
+        # Check if mutation is reflected in database by doing a database query.
         account_2 = Account.objects.get(current_balance=200)
-        self.assertTrue(account_1.disabled == False)
+        self.assertFalse(account_1.disabled)
 
-# Test to confirm if transactions connected to an account are deleted after said account is disabled. 
+
 class EnableDisableAccountsTransactionsTestCase(SWTestCase):
+    'Test to confirm if transactions connected to account are deleted after account is disabled.'
+
     def test_accounts_transactions_after_enable_and_disable(self):
         owner = UserFactory.create()
         account = AccountFactory.create(owner=owner)
-        transaction = TransactionFactory.create(owner=owner, account=account, description="t1 1", amount=Decimal(150))
+        TransactionFactory.create(owner=owner, account=account, amount=Decimal(150))
 
         # GraphQL string input for querying transactions. Stored for readability and repeated use.
         transactions_graph_query = '''{
@@ -199,9 +228,12 @@ class EnableDisableAccountsTransactionsTestCase(SWTestCase):
             }
         }'''
 
-        transaction_result = self.graph_query(transactions_graph_query, user=owner).data['viewer']['transactions']['edges']
+        transaction_result = self.graph_query(
+            transactions_graph_query,
+            user=owner,
+        ).data['viewer']['transactions']['edges']
 
-        # Confirm if singular transaction exists after TransactionFactory.create().
+        # Check if singular transaction exists after TransactionFactory.create().
         self.assertTrue(len(transaction_result) == 1)
         self.assertTrue(transaction_result[0]['node']['amount'] == 15000)
 
@@ -229,11 +261,14 @@ class EnableDisableAccountsTransactionsTestCase(SWTestCase):
             user=owner,
         )
 
-        transaction_result = self.graph_query(transactions_graph_query, user=owner).data['viewer']['transactions']['edges']
+        transaction_result = self.graph_query(
+            transactions_graph_query,
+            user=owner,
+        ).data['viewer']['transactions']['edges']
 
-        # Confirm that there are no transactions using GraphQL after the account it was connected to is disabled.
+        # Check that there are no transactions using GraphQL after connected account.
         self.assertTrue(len(transaction_result) == 0)
 
-        # Check if GraphQL disableAccount mutation is reflected in database by doing a database query.
+        # Check if disableAccount mutation is reflected in database by doing a database query.
         account = Account.objects.get(owner=owner)
-        self.assertTrue(account.disabled == True)       
+        self.assertTrue(account.disabled is True)
