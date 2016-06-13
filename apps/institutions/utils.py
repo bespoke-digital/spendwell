b4f064d2f3bb9b806633631e6d05cacb69548311
Aron Jones <aron.jones@gmail.com>
2016-06-13 14:15:23 -0400

    don't use chords, since they seem sketchy

--- apps/institutions/utils.py
+++ apps/institutions/utils.py
@@ -1,5 +1,5 @@
 
-from celery import chain, group, chord
+from celery import chain, group
 
 from apps.transactions.tasks import detect_transfers
 from apps.buckets.tasks import assign_bucket_transactions, autodetect_bills as autodetect_bills_task
@@ -10,22 +10,22 @@ from .tasks import sync_institution
 
 
 def sync_user(owner, estimate_income=False, autodetect_bills=False):
-    syncs = (
+    syncs = group(
         sync_institution.si(id)
         for id in owner.institutions.values_list('id', flat=True)
     )
 
-    after_sync = [detect_transfers.si(owner.id)]
+    chain_tasks = [syncs, detect_transfers.si(owner.id)]
 
     if autodetect_bills:
-        after_sync.append(autodetect_bills_task.si(owner.id))
+        chain_tasks.append(autodetect_bills_task.si(owner.id))
 
-    after_sync.append(assign_bucket_transactions.si(owner.id))
+    chain_tasks.append(assign_bucket_transactions.si(owner.id))
 
     if estimate_income:
-        after_sync.append(estimate_income_task.si(owner.id))
+        chain_tasks.append(estimate_income_task.si(owner.id))
 
-    return chord(syncs, chain(after_sync))
+    return chain(chain_tasks)
 
 
 def sync_all():
