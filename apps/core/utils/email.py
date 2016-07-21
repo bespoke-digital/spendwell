14866f56d03b93505d0ca4a54f2e17f34d4d1ade
Aron Jones <aron.jones@gmail.com>
2016-07-21 12:34:47 -0400

    mixpanel based email open tracking

--- apps/core/utils/email.py
+++ apps/core/utils/email.py
@@ -1,4 +1,7 @@
 
+import json
+from base64 import b64encode
+
 from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.template.loader import render_to_string
@@ -34,6 +37,17 @@ def render_email(subject, user, template, context=None):
         'user_id': user.id,
         'signature': unsubscribe_sign(user),
     })
+
+    context['open_event_properties'] = context.get('open_event_properties', {})
+    context['open_event_image'] = 'http://api.mixpanel.com/track/?data={}&ip=1&img=1'.format(
+        b64encode(json.dumps({
+            'event': 'Email Open',
+            'properties': context['open_event_properties'].update({
+                'distinct_id': '{}'.format(user.id),
+                'token': settings.MIXPANEL_PUBLIC_KEY,
+            }),
+        }).encode('utf-8')).decode('utf-8')
+    )
 
     html_message = render_to_string(template, context)
     html_message = premailer.transform(
